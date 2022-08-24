0578e180142e2b38409dc5c6d70f2e6579fb93bd
DESKTOP-3BPMP1N\leemo <leemong486@naver.com>
2022-08-24 09:40:53 +0900

    필요없는페이지 및 상품등록 페이지 추가

--- src/main/webapp/login/sellpage/sellpage.js
+++ src/main/webapp/login/sellpage/sellpage.js
@@ -1,14 +1,21 @@
 /**
  * 
  */
- 
- //sellpage
+
+//sellpage
 $(function() {
+	$("#submit").click(function() {
+		$("#sell-submit").show();
+		$("#sell-list").hide();
+		$("#sell-control").hide();
+	});
 	$("#control").click(function() {
+		$("#sell-submit").hide();
 		$("#sell-list").hide();
 		$("#sell-control").show();
 	});
 	$("#list").click(function() {
+		$("#sell-submit").hide();
 		$("#sell-control").hide();
 		$("#sell-list").show();
 	});

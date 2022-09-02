fe3c9e2dfb03d69d412075e545bcf97834c70a23
JunQ <junkyu970307@gmail.com>
2022-09-02 17:50:58 +0900

    아이디 중복체크 완료

---
+++ src/main/java/com/spring/myweb/DAO/RegisterAgreeDAO/RegisterAgreementDAOImpl.java
@@ -0,0 +1,30 @@
+package com.spring.myweb.DAO.RegisterAgreeDAO;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.spring.myweb.VO.RegisterAgreementVO.RegisterAgreementVO;
+
+@Repository
+public class RegisterAgreementDAOImpl implements RegisterAgreementDAO{
+
+	@Inject
+	private SqlSession session;
+	
+	@Override
+	public List<RegisterAgreementVO> selectAll() {
+		List<RegisterAgreementVO> agreementList = session.selectList("adminDB.selectAgreementAll");
+		return agreementList;
+	}
+
+	@Override
+	public RegisterAgreementVO select(int agreement_seq) {
+		RegisterAgreementVO agreement = session.selectOne("adminDB.selectAgreement",agreement_seq);
+		return agreement;
+	}
+
+}
